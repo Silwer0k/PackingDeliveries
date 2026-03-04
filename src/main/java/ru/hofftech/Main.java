3a6aded42598083ccb4ca53c1f752105cf234b80
Egor Yakubovich <Egor.Yakubovich@hofftech.ru>
2026-03-04 11:24:57 +0300

    Добавил второй аргумент типа упаковки, factory, enum создания Packager'ов

--- src/main/java/ru/hofftech/Main.java
+++ src/main/java/ru/hofftech/Main.java
@@ -1,30 +1,34 @@
 package ru.hofftech;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.hofftech.packingdeliveries.model.CargoPackage;
-import ru.hofftech.packingdeliveries.service.OneByOnePackager;
+import ru.hofftech.packingdeliveries.model.PackagerFactory;
 import ru.hofftech.packingdeliveries.service.PackageReader;
 import ru.hofftech.packingdeliveries.service.Packager;
-import ru.hofftech.packingdeliveries.service.SmartPackager;
 
 import java.util.ArrayList;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        if (args.length != 0){
+        if (args.length == 2){
             String packagesFileName = args[0];
-            ArrayList<CargoPackage> packagesFromFile = new PackageReader().readFromFile(packagesFileName);
-
-            Packager oneByOnePackager = new OneByOnePackager();
-            oneByOnePackager.doPacking(packagesFromFile);
-            oneByOnePackager.showPackingResults();
-
-            Packager smartPackager = new SmartPackager();
-            smartPackager.doPacking(packagesFromFile);
-            smartPackager.showPackingResults();
+            String packingMethod = args[1];
+            try {
+                Packager packager = new PackagerFactory().construct(packingMethod);
+                ArrayList<CargoPackage> packagesFromFile = new PackageReader().readFromFile(packagesFileName);
+                packager.doPacking(packagesFromFile);
+                packager.showPackingResults();
+            }
+            catch (IllegalArgumentException exception)
+            {
+                log.error("Метод упаковки {} не поддерживается!", packingMethod);
+            }
         }
         else {
-            System.out.println("Не указан путь к файлу с посылками для упаковки!");
+            log.error("Необходимо указать путь к файлу с посылками и методом упаковки в грузовики!");
         }
     }
 }
---
+++ src/main/java/ru/hofftech/packingdeliveries/model/PackagingMethod.java
@@ -0,0 +1,6 @@
+package ru.hofftech.packingdeliveries.model;
+
+public enum PackagingMethod {
+    ONEBYONE,
+    SMART
+}
